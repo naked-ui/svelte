7e1df19c797496443d11cad4729b25919fa5a78b
Aleksander Grygier <alek@newfantastic.com>
2021-07-23 11:15:00 +0200

    chore: Test publication changes

--- src/components/NuiButton/NuiButton.svelte
+++ src/components/NuiButton/NuiButton.svelte
@@ -14,23 +14,12 @@
 {/if}
 
 <style>
-	@import 'sxcss';
 	button,
 	a {
-		/* padding: space(4) space(6); */
 		background: var(--background);
 		appearance: none;
 		text-decoration: none;
 		color: var(--color, #fff);
 		transition: var(--transition-1);
-		/* border-radius: space(12); */
-		/* @include font(17px, 500); */
-
-		/* &:hover {
-			@media (min-width: 768px) {
-				background: var(--background-hover, var(--background));
-				transform: translate(0, space(-1));
-			}
-		} */
 	}
 </style>
